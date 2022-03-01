5e47ed031fb791db518278edbb850191c127fbc9
william <will.gauvin@gmail.com>
2022-02-28 20:09:33 -0500

    setup

---
+++ bochandail/pages/index.js
@@ -0,0 +1,14 @@
+import { Heading, Page, Button } from "@shopify/polaris";
+
+export default function Index() {
+  return (
+    <Page>
+      <Heading>
+        Shopify app with Node and React{" "}
+        <span role="img" aria-label="tada emoji">
+          🎉
+        </span>
+      </Heading>
+    </Page>
+  );
+}
